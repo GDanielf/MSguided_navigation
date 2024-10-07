0c8a932b253e19fc28e32332fc17b0f3aa3464ba
Gustavo Daniel <gudanielf@gmail.com>
2024-10-07 17:21:00 -0300

    caso de ruim, volta pra esse

--- src/triangulation.py
+++ src/triangulation.py
@@ -12,26 +12,22 @@ class Triangulation(Node):
         super().__init__('triangulation')
 
         #Posicoes e rotacoes das cameras no mundo
-        self.camera0_pos = np.array([9.9253, -7.43722])
+        self.camera0_pos = np.array([9.9843, -7.4687])
         self.camera0_rot = np.array([-0.15493676217535018, 0.051481367426355791, 0.93625125233565687])
-        self.camera1_pos = np.array([-9.9253, -7.43722])
+        self.camera1_pos = np.array([-9.9253, -7.4687])
         self.camera1_rot = np.array([-0.052132214164251366, 0.1547189830363459, 0.315024337110333])
-        self.camera2_pos = np.array([9.9253, 7.43722])
+        self.camera2_pos = np.array([9.9253, 7.4372])
         self.camera2_rot = np.array([0.15493676217535018, 0.051481367426355791, -0.93625125233565687])
-        self.camera3_pos = np.array([-9.9253, 7.43722])
+        self.camera3_pos = np.array([-9.9843, 7.4687])
         self.camera3_rot = np.array([0.052132214164251366, 0.1547189830363459, -0.315024337110333])
-        self.camera4_pos = np.array([-3.8126, -2.3696])
+        self.camera4_pos = np.array([-6.8126, -2.3696])
         self.camera4_rot = np.array([-0.18549626982220258, 0.031907839433870766, 0.96791159620266265])
-        self.camera5_pos = np.array([-3.8126, 2.3696])
+        self.camera5_pos = np.array([-6.8126, 2.3696])
         self.camera5_rot = np.array([0.18549626982220258, 0.031907839433870766, -0.96791159620266265])
-        self.camera6_pos = np.array([3.8126, 2.3696])
+        self.camera6_pos = np.array([6.8126, 2.3696])
         self.camera6_rot = np.array([0.031908520799500469, 0.18549615261691255, -0.16649729576294525])
-        self.camera7_pos = np.array([3.8126, -2.3696])
+        self.camera7_pos = np.array([6.8126, -2.3696])
         self.camera7_rot = np.array([-0.031908520799500469, 0.18549615261691255, 0.16649729576294525])
-        self.camera8_pos = np.array([0, -7.4687])
-        self.camera8_rot = np.array([-0.15452609606586357, 0.15464919835800375, 0.689727136104762])
-        self.camera9_pos = np.array([0, 7.4687])
-        self.camera9_rot = np.array([0.15452609606586357, 0.15464919835800375, -0.689727136104762])
         self.hfov = 1.0469999999999999
         self.camera_position_vec = 2
         self.image_position_vec = 30        
@@ -70,10 +66,6 @@ class Triangulation(Node):
             return (a >= round(camera_list[6][0],2) and b <= round(camera_list[6][1],2))
         elif camera_desejada == 7:
             return (a >= round(camera_list[7][0],2) and b >= round(camera_list[7][1],2))
-        elif camera_desejada == 8:
-            return (b >= round(camera_list[8][1],2))
-        elif camera_desejada == 9:
-            return (b <= round(camera_list[9][1],2))
     
     
     def intersecao_retas(self, A1, B1, C1, A2, B2, C2, camera_list, camera1, camera2):
@@ -122,8 +114,7 @@ class Triangulation(Node):
     def triangulation_callback(self, msg):
         mapa = Mapa()
         camera_position = [self.camera0_pos, self.camera1_pos, self.camera2_pos, self.camera3_pos,
-                           self.camera4_pos, self.camera5_pos, self.camera6_pos, self.camera7_pos, 
-                           self.camera8_pos, self.camera9_pos]
+                           self.camera4_pos, self.camera5_pos, self.camera6_pos, self.camera7_pos]
         camera_rotations = [self.yaw_rotation(self.camera0_rot[0], self.camera0_rot[1], self.camera0_rot[2]), 
                      self.yaw_rotation(self.camera1_rot[0], self.camera1_rot[1], self.camera1_rot[2]),
                      self.yaw_rotation(self.camera2_rot[0], self.camera2_rot[1], self.camera2_rot[2]),
@@ -131,9 +122,7 @@ class Triangulation(Node):
                      self.yaw_rotation(self.camera4_rot[0], self.camera4_rot[1], self.camera4_rot[2]),
                      self.yaw_rotation(self.camera5_rot[0], self.camera5_rot[1], self.camera5_rot[2]),
                      self.yaw_rotation(self.camera6_rot[0], self.camera6_rot[1], self.camera6_rot[2]),
-                     self.yaw_rotation(self.camera7_rot[0], self.camera7_rot[1], self.camera7_rot[2]),
-                     self.yaw_rotation(self.camera8_rot[0], self.camera8_rot[1], self.camera8_rot[2]),
-                     self.yaw_rotation(self.camera9_rot[0], self.camera9_rot[1], self.camera9_rot[2])]
+                     self.yaw_rotation(self.camera7_rot[0], self.camera7_rot[1], self.camera7_rot[2])]
 
         
         #print('Angle image 0: ', msg.angle_image_0, 'Angle image 1: ', msg.angle_image_1, 'Angle image 2: ', msg.angle_image_2, 'Angle image 3: ', msg.angle_image_3)
@@ -183,8 +172,6 @@ class Triangulation(Node):
         hfov_limit[5] = [-1.57, -3.1416]
         hfov_limit[6] = [0, -1.57]
         hfov_limit[7] = [1.57, 0]
-        hfov_limit[8] = [camera_rotations[8] + self.hfov, camera_rotations[8] - self.hfov]
-        hfov_limit[9] = [camera_rotations[9] + self.hfov, camera_rotations[9] - self.hfov]
 
         #plotando os limites de hfov
         for pos, angle in zip(camera_position, hfov_limit):
@@ -203,8 +190,7 @@ class Triangulation(Node):
             ax.scatter(pos[0], pos[1], color='b') 
         
         image_angles = [msg.angle_image_0, msg.angle_image_1, msg.angle_image_2, msg.angle_image_3,
-                         msg.angle_image_4, msg.angle_image_5, msg.angle_image_6, msg.angle_image_7,
-                         msg.angle_image_8, msg.angle_image_9]
+                         msg.angle_image_4, msg.angle_image_5, msg.angle_image_6, msg.angle_image_7]
 
         image_angles_res = []
 
