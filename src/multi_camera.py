7b3ae3b30b43d196dcbf2108f8a4f7719df20d98
gustavo <gudanielf@gmail.com>
2025-02-11 21:39:34 -0300

    triangulacao feita no multi camera

--- src/multi_camera.py
+++ src/multi_camera.py
@@ -4,7 +4,6 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from std_msgs.msg import Bool, Float64MultiArray
-from guided_navigation.msg import ImagesAngles
 from guided_navigation.srv import SetCameraActive
 from cv_bridge import CvBridge
 import cv2
@@ -14,7 +13,8 @@ import math
 from message_filters import Subscriber, TimeSynchronizer
 from rosgraph_msgs.msg import Clock
 from sensor_msgs.msg import JointState
-
+from guided_navigation.msg import PoseEstimate
+from scipy.spatial.transform import Rotation as rot
 
 #from guided_navigation.msg import Rectangle
 class MultiCamera(Node):
@@ -39,6 +39,15 @@ class MultiCamera(Node):
         self.end_point_horiozontal = (self.image_width, self.mid_y)
         self.line_color = (0, 0, 0)
         self.thickness = 2  
+        #parametros de triangulacao
+        self.camera0_pos = np.array([-3, -7.4687]) #rot_z = 1.57
+        self.camera0_rot = np.array([0, 0, np.pi/2]) #rot z,y
+        self.camera1_pos = np.array([4.0, 7.4687])
+        self.camera1_rot = np.array([0, 0, -np.pi/2])
+        self.camera2_pos = np.array([-9.9830, -1.2500])
+        self.camera2_rot = np.array([0, 0, 0])  
+        self.pose_x = 0
+        self.pose_y = 0
 
         # Configuração do Modelo
         self.model = inference.get_model("husky_test/3")
@@ -53,8 +62,7 @@ class MultiCamera(Node):
             Clock, '/clock', self.clock_callback, 10  
         )
 
-        #angle_publisher para enviar os valores dos angulos timer_publition publica msg a cada 1 seg
-        self.angle_publisher = self.create_publisher(ImagesAngles, 'image_angles', 10)
+        self.pose_publisher = self.create_publisher(PoseEstimate, 'pose_estimate', 10)                  
 
         #joints
         self.velocity_publishers = {
@@ -74,17 +82,17 @@ class MultiCamera(Node):
         ]
         self.initial_target_position = [0.5, 0.5, 0.4]
         self.kp = 1
-        self.kp_cam_yaw = 0.0001
-        self.kp_cam_tilt = 0.0001
+        self.kp_cam_yaw = 0.00025
+        self.kp_cam_tilt = 0.00025
         self.threshold = 0.01
         self.threshold_pixel = 2
         self.iniciar_posicao = [False, False, False]
+        self.joint_imagem_centralizada = [[0.0, 0.0],[0.0, 0.0],[0.0, 0.0]] #index 0 = camera 0 [yaw,tilt]
         self.joint_positions = {}
         self.joints_to_control = ["camera_joint_0", "camera_joint_1", "camera_joint_2"]
         self.x_centro = 1920 // 2
         self.y_centro = 1080 // 2
 
-        self.angles = [float('nan')] * 3 
         self.active_cameras = {i: False for i in range(3)}     
 
         self.robot_moving = False 
@@ -114,16 +122,22 @@ class MultiCamera(Node):
         self.srv = self.create_service(SetCameraActive, 'set_camera_active', self.set_camera_active)
         
     def clock_callback(self, msg):
-        # Recebe o tempo de simulação
         self.simulation_time = msg.clock
+
+    def publish_pose_estimate(self):
+        msg = PoseEstimate()
+        msg.x = float(self.pose_x)  
+        msg.y = float(self.pose_y) 
+        self.pose_publisher.publish(msg) 
 
     def simulation_callback(self, msg):
         self.simulation_active = msg.data
 
     def robot_moving_callback(self, msg):
         self.robot_moving = msg.data
-        if not msg.data:  
+        if not msg.data and all(self.iniciar_posicao):  
             self.robot_stopped_time = self.simulation_time
+            self.publish_camera_angles()            
 
     def joint_state_callback(self, msg):
         for i, name in enumerate(msg.name):
@@ -133,6 +147,25 @@ class MultiCamera(Node):
             if not self.iniciar_posicao[index]:
                 self.inicializar_posicao_y_joint(joint_name, index)
                 break
+        if(all(self.iniciar_posicao)): # esse recebe o valor do planner
+            pontos_estimados = []
+            camera_position = [self.camera0_pos, self.camera1_pos, self.camera2_pos]                           
+             
+            rot_0 = rot.from_euler('xyz', [self.camera0_rot[0], self.camera0_rot[1] + self.joint_imagem_centralizada[0][1], self.camera0_rot[2] + self.joint_imagem_centralizada[0][0]])
+            rot_1 = rot.from_euler('xyz', [self.camera1_rot[0], self.camera1_rot[1] + self.joint_imagem_centralizada[1][1], self.camera1_rot[2] + self.joint_imagem_centralizada[1][0]])
+            rot_2 = rot.from_euler('xyz', [self.camera2_rot[0], self.camera2_rot[1] + self.joint_imagem_centralizada[2][1], self.camera2_rot[2] + self.joint_imagem_centralizada[2][0]])           
+                
+            camera_rotations = [rot_0.as_euler('xyz')[2], rot_1.as_euler('xyz')[2], rot_2.as_euler('xyz')[2]]
+            
+            print(camera_rotations)
+            for i in range(3):
+                for j in range(i + 1, 3):
+                    pontos_estimados.append(self.estimate_pose(camera_position[i], camera_position[j], camera_rotations[i], camera_rotations[j])) 
+
+            pontos_medio_x, pontos_medio_y = self.pontos_medio(pontos_estimados)
+            self.pose_x = float(pontos_medio_x)
+            self.pose_y = float(pontos_medio_y)  
+            self.publish_pose_estimate()        
 
     def inicializar_posicao_y_joint(self, joint_name, index):
         velocity_msg = Float64MultiArray()
@@ -149,18 +182,7 @@ class MultiCamera(Node):
         self.velocity_publishers[self.joints_to_control[index]].publish(velocity_msg)
 
         self.get_logger().info(f"Ajustando {joint_name}: pos {current_pos:.3f} → vel {velocity:.3f}")
-
-
-    def publish_camera_angles(self):
-        msg = ImagesAngles()
-        msg.angles = self.angles
-        self.angle_publisher.publish(msg)
-    
-    #funcao que recebe o ponto do centro do retangulo e retorna o angulo entre a reta que divide metade da imagem o ponto 
-
-    def angulo_centro(self,x,y):        
-        return math.atan((x - 959)/(1080 - y))
-
+ 
     def camera_callback(self, *camera_images):
         velocity_msg = Float64MultiArray()
         if all(self.iniciar_posicao):
@@ -189,19 +211,61 @@ class MultiCamera(Node):
                         if abs(erro_x) < self.threshold_pixel and abs(erro_y) < self.threshold_pixel:
                             velocity_msg.data = [0.0, 0.0]
                             self.velocity_publishers[self.joints_to_control[camera_id]].publish(velocity_msg)
-                        
-                        velocity_msg.data = [float(-self.kp_cam_yaw * erro_x), float(self.kp_cam_tilt * erro_y)]
-                        self.velocity_publishers[self.joints_to_control[camera_id]].publish(velocity_msg)
+                            self.joint_imagem_centralizada[camera_id] =  [self.joint_positions[self.yaw_joints[camera_id]], 
+                                                                    self.joint_positions[self.tilt_joints[camera_id]]]
+                            #print(self.joint_imagem_centralizada)
+                        else:                       
+                            #self.joint_imagem_centralizada[camera_id] = [float('nan'), float('nan')]
+                            velocity_msg.data = [float(-self.kp_cam_yaw * erro_x), float(self.kp_cam_tilt * erro_y)]
+                            self.velocity_publishers[self.joints_to_control[camera_id]].publish(velocity_msg)
                 
                 # Apenas exibir se a câmera for ativada
                 if self.is_camera_active(camera_id):
                     self.visualize_camera(cv_image, camera_id)
-                
-            if(self.simulation_active and not self.robot_moving): 
-                self.publish_camera_angles()    
-        
-            
 
+    #triangulacao
+    
+    def pontos_medio(self, lista_pontos_estimados):
+        x = 0
+        y = 0
+        if(lista_pontos_estimados != []):
+            for i in range(len(lista_pontos_estimados)):
+                x = x + lista_pontos_estimados[i][0]
+                y = y + lista_pontos_estimados[i][1]        
+            x = x/(len(lista_pontos_estimados))
+            y = y/(len(lista_pontos_estimados))
+        return x,y 
+    
+    def estimate_pose(self, camera1_pos, camera2_pos, angle1, angle2):
+        distance_vector = camera2_pos - camera1_pos
+        d12 = np.linalg.norm(distance_vector)
+        detection_vector1 = np.array([np.cos(angle1), np.sin(angle1)])
+        detection_vector2 = np.array([np.cos(angle2), np.sin(angle2)])
+        # Ângulo entre o vetor de detecção da câmera e o vetor distância
+        angle_internal1 = np.arccos(
+            np.dot(detection_vector1, distance_vector) / (np.linalg.norm(detection_vector1) * d12)
+        )
+        angle_internal2 = np.arccos(
+            np.dot(detection_vector2, -distance_vector) / (np.linalg.norm(detection_vector2) * d12)
+        )
+        angle3 = np.pi - angle_internal1 - angle_internal2
+
+        sin_angle1 = np.sin(angle_internal1)
+        sin_angle2 = np.sin(angle_internal2)
+        sin_angle3 = np.sin(angle3)
+
+        A = (d12 * sin_angle2)/(sin_angle3)
+        B = (d12 * sin_angle1)/(sin_angle3)
+
+        est_1 = camera1_pos + A * detection_vector1
+        est_2 = camera2_pos + B * detection_vector2
+
+        result = (est_1 + est_2)/2
+
+        return result 
+                
+            
+    #servico para ativar as cameras com opencv
     def set_camera_active(self, request, response):
         camera_id = request.camera_id
         activate = request.activate
